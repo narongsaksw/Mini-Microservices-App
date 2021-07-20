115d2d801c280cb2c6496856a3b0d3ef8c9ddf1b
sscarry2 <earth.narongsak@gmail.com>
2021-07-20 23:58:20 +0700

    implementing event sync

--- query/index.js
+++ query/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,13 +8,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
 
@@ -38,12 +33,31 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
-  console.log(posts);
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
 
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Listening on 4002");
+
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
