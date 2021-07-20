a1be11985b88be66648d406b287d03bd1d8cec7c
sscarry2 <earth.narongsak@gmail.com>
2021-07-20 23:34:24 +0700

    moderation service

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,7 +7,8 @@ import CommentList from "./CommentList";
 const PostList = () => {
   const [posts, setPosts] = useState({});
   const getPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
+    const res = await axios.get("http://localhost:4002/posts");
+    console.log(res.data);
     setPosts(res.data);
   };
   useEffect(() => {
@@ -23,7 +24,7 @@ const PostList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
